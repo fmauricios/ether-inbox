7c547f10a09469e14d0d0482df376662b46abe39
Mauricio Serna Florez <mauricioserf@gmail.com>
2018-03-05 09:11:28 -0500

    [Add] deploy contract

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -6,8 +6,10 @@ const assert = require('assert')
 const ganache = require('ganache-cli')
 const Web3 = require('web3')
 const web3 = new Web3(ganache.provider())
+const { interface, bytecode } = require('../compile')
 
 let accounts = null
+let inbox = null
 
 beforeEach(async () => {
   /*
@@ -15,12 +17,16 @@ beforeEach(async () => {
    * 2. Use one of those accounts to deploy the contract
    */
 
-   accounts = await web3.eth.getAccounts()
+  accounts = await web3.eth.getAccounts()
 
-   console.log(accounts)
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi Ethereum.'] })
+    .send({ from: accounts[0], gas: '1000000' })
+   
 })
 
 describe('Inbox', () => {
   it('deploys a contract', () => {
+    console.log(inbox)
   })
 })

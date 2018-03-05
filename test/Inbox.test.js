7e510b9984c11373eb7dd59916037c42858623a4
Mauricio Serna Florez <mauricioserf@gmail.com>
2018-03-05 09:01:43 -0500

    [Add] async/await

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -7,16 +7,17 @@ const ganache = require('ganache-cli')
 const Web3 = require('web3')
 const web3 = new Web3(ganache.provider())
 
-beforeEach(() => {
+let accounts = null
+
+beforeEach(async () => {
   /*
    * 1. Get a list of all accounts
    * 2. Use one of those accounts to deploy the contract
    */
 
-   web3.eth.getAccounts()
-    .then(fetchedAccounts => {
-      console.log(fetchedAccounts)
-    })
+   accounts = await web3.eth.getAccounts()
+
+   console.log(accounts)
 })
 
 describe('Inbox', () => {

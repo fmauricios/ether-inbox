9ec3ae1c205810b6002111760da457599d66aa20
Mauricio Serna Florez <mauricioserf@gmail.com>
2018-03-05 08:57:48 -0500

    [Add] basic fetch accounts test

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,4 +1,25 @@
+/*
+ * Module dependencies
+ */
+
 const assert = require('assert')
 const ganache = require('ganache-cli')
 const Web3 = require('web3')
 const web3 = new Web3(ganache.provider())
+
+beforeEach(() => {
+  /*
+   * 1. Get a list of all accounts
+   * 2. Use one of those accounts to deploy the contract
+   */
+
+   web3.eth.getAccounts()
+    .then(fetchedAccounts => {
+      console.log(fetchedAccounts)
+    })
+})
+
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+  })
+})
